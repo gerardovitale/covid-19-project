cffe63c0cc013bfd62644e4f589c1db9414f5d27
Gerardo Vitale <gerardo.vitale@thoughtworks.com>
2022-05-27 18:18:20 +0200

    added pylint and flake8 steps

--- data_pipeline/entrypoint.sh
+++ data_pipeline/entrypoint.sh
@@ -3,5 +3,14 @@
 # run tests
 python -m unittest discover $CONTAINER_BASE_DIR/tests || exit 1
 
+# Analysing the code with pylint
+pylint --max-line-length=120 \
+    --output-format=colorized \
+    $CONTAINER_BASE_DIR/pipeline $CONTAINER_BASE_DIR/tests
+
+# Lint with flake8
+flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics
+flake8 . --count --exit-zero --max-complexity=10 --max-line-length=120 --statistics
+
 # execute pipeline
 python -u $CONTAINER_BASE_DIR/pipeline/pipeline.py
--- data_pipeline/src/pipeline/extract.py
+++ data_pipeline/src/pipeline/extract.py
@@ -4,6 +4,6 @@ import pandas as pd
 
 def extract_data(chunk: bool = False) -> pd.DataFrame:
     DATA_URL = os.getenv('DATA_URL')
-    chunksize = 1000 if chunk else None 
+    chunksize = 1000 if chunk else None
     data = pd.read_csv(DATA_URL, nrows=chunksize)
     return data
