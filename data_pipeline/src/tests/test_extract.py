bc2e824dcf8bf1213d9443ad0f5c70d7c0e70625
Gerardo Vitale <gerardo.vitale.errico@gmail.com>
2022-05-28 13:02:23 +0200

    test case added for chunksize==false

--- data_pipeline/src/tests/test_extract.py
+++ data_pipeline/src/tests/test_extract.py
@@ -1,16 +1,29 @@
 from unittest import TestCase
+
 from pandas import DataFrame
 
-from tests.resources import EXPECTED_FEATURES
 from pipeline.extract import extract_data
+from tests.resources import EXPECTED_FEATURES
 
 
 class TestExtract(TestCase):
 
     def test_extract_goes_well(self):
+        expected_rows = 1000
+
         data = extract_data(chunk=True)
 
-        acutal_features = data.columns
+        self.assertIsInstance(data, DataFrame)
+        self.assertTrue((EXPECTED_FEATURES == data.columns).all())
+        self.assertEqual(expected_rows, data.shape[0])
+
+    def test_extract_when_chunk_false(self):
+        expected_rows = 178468
+        expected_cols = 67
+
+        data = extract_data(chunk=False)
 
         self.assertIsInstance(data, DataFrame)
-        self.assertTrue((EXPECTED_FEATURES == acutal_features).all())
+        self.assertTrue((EXPECTED_FEATURES == data.columns).all())
+        self.assertTrue(expected_rows <= data.shape[0])
+        self.assertEqual(expected_cols, data.shape[1])
