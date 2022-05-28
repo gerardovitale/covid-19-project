579a647099211b9547d4f168bf31c7c631c42f7d
Gerardo Vitale <gerardo.vitale.errico@gmail.com>
2022-05-28 13:02:50 +0200

    simplified unittest

--- data_pipeline/src/tests/test_pipeline.py
+++ data_pipeline/src/tests/test_pipeline.py
@@ -1,13 +1,10 @@
 from unittest import TestCase
-from unittest.mock import patch
-from unittest.mock import Mock
 
 from pipeline.pipeline import run_pipeline
 
 
 class TestPipeline(TestCase):
 
-    @patch('pipeline.pipeline.run_pipeline')
-    def test_pipeline(self, pipeline_mock: Mock):
+    def test_pipeline(self):
         run_pipeline()
         self.assertTrue(True)
