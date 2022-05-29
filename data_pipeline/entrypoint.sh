0528ef820279a73ea3e24c0de98f5be98a66f468
Gerardo Vitale <gerardo.vitale.errico@gmail.com>
2022-05-29 14:29:55 +0200

    some prints added

--- data_pipeline/entrypoint.sh
+++ data_pipeline/entrypoint.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
 # run tests
+echo "***************************************************************************"
+echo "[INFO] running python unittest"
 python -m unittest discover "$CONTAINER_BASE_DIR"/tests || exit 1
 
 # Analysing the code with pylint
+echo "***************************************************************************"
+echo "[INFO] running pylint"
 pylint --max-line-length=120 --output-format=colorized \
     "$CONTAINER_BASE_DIR"/pipeline "$CONTAINER_BASE_DIR"/tests
 
 # Lint with flake8
+echo "***************************************************************************"
+echo "[INFO] running flake8"
 flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics
 flake8 . --count --exit-zero --max-complexity=10 --max-line-length=120 --statistics
 
 # execute pipeline
+echo "***************************************************************************"
+echo "[INFO] executing data pipeline"
 python -u "$CONTAINER_BASE_DIR"/pipeline/pipeline.py
