00f6015883c6b6d686dc50f51efdf68164ef8064
Gerardo Vitale <gerardo.vitale.errico@gmail.com>
2022-06-01 20:04:24 +0200

    pipeline updated

--- data_pipeline/src/pipeline/pipeline.py
+++ data_pipeline/src/pipeline/pipeline.py
@@ -1,6 +1,14 @@
+from pipeline.config.config import DataBaseConfig
 from pipeline.config.logger import get_logger
+from pipeline.extract import extract_data
+from pipeline.load import Loader
+from pipeline.transform import Transformer
 
 
 def run_pipeline() -> None:
     logger = get_logger('Pipeline')
-    logger.info('this is the pipeline writing from the logger')
+    logger.debug('this is the pipeline writing from the logger')
+
+    data = extract_data()
+    new_cases = Transformer.get_new_cases_per_month_n_location(data)
+    Loader(DataBaseConfig()).publish_record_list_to_db(new_cases)
--- data_pipeline/src/tests/test_pipeline.py
+++ data_pipeline/src/tests/test_pipeline.py
@@ -1,10 +1,25 @@
 from unittest import TestCase
+from unittest.mock import patch
 
+from pipeline.load import Loader
 from pipeline.pipeline import run_pipeline
+from pipeline.transform import Transformer
 
 
 class TestPipeline(TestCase):
+    def setUp(self) -> None:
+        extract_data_patcher = patch('pipeline.extract.extract_data')
+        transformer_patcher = patch.object(Transformer, 'get_new_cases_per_month_n_location')
+        loader_patcher = patch.object(Loader, 'publish_record_list_to_db')
+        self.addCleanup(extract_data_patcher.stop)
+        self.addCleanup(transformer_patcher.stop)
+        self.addCleanup(loader_patcher.stop)
+        self.mock_extract = extract_data_patcher.start()
+        self.mock_transformer = transformer_patcher.start()
+        self.mock_loader = loader_patcher.start()
 
     def test_pipeline(self):
         run_pipeline()
-        self.assertTrue(True)
+
+        self.mock_transformer.assert_called_once()
+        self.mock_loader.assert_called_once()
