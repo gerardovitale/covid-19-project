a6424f55c8b95e7d6567d3a75e8b93ab0aea402f
Gerardo Vitale <gerardo.vitale.errico@gmail.com>
2022-06-01 20:01:57 +0200

    extract_data refactored

--- data_pipeline/src/pipeline/extract.py
+++ data_pipeline/src/pipeline/extract.py
@@ -2,9 +2,12 @@ import os
 
 import pandas as pd
 
+from pipeline.config.logger import get_logger
+
+logger = get_logger('Extractor')
+
 
 def extract_data(chunk: bool = False) -> pd.DataFrame:
-    DATA_URL = os.getenv('DATA_URL')
     chunksize = 1000 if chunk else None
-    data = pd.read_csv(DATA_URL, nrows=chunksize)
-    return data
+    logger.debug('Extracting data')
+    return pd.read_csv(os.getenv('DATA_URL'), nrows=chunksize)
