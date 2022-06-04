f9dfd3e21b230ff597d7f649a3f552306f1c2689
Gerardo Vitale <gerardo.vitale.errico@gmail.com>
2022-06-04 21:20:48 +0200

    api connected to mongo atlas db

--- api/src/main.go
+++ api/src/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"context"
 	"covid-19-project/pkg/app"
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 func main() {
@@ -17,6 +22,25 @@ func main() {
 }
 
 func run() error {
+	uri := os.Getenv("MONGO_PASS")
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	// Ping the primary
+	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
+		panic(err)
+	}
+	log.Println("Successfully connected and pinged to Mongo Atlas DB")
+
 	// create router dependency
 	router := gin.Default()
 	router.Use(cors.Default())
@@ -24,7 +48,7 @@ func run() error {
 	server := app.NewServer(router)
 
 	// start the server
-	err := server.Run()
+	err = server.Run()
 
 	if err != nil {
 		return err
