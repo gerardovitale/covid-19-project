b4a29bcabdf2245a8963c8f600cfa555eaabc182
Gerardo Vitale <gerardo.vitale@thoughtworks.com>
2022-06-06 17:50:44 +0200

    added mising -

--- commons.sh
+++ commons.sh
@@ -10,12 +10,13 @@ cleanUpDocker() {
 
   containerRunning=$(docker ps | grep $1)
   if [[ $containerRunning ]]; then
-      echo "$(dateTimeNow) [INFO] - Stopping $1 container"
-      docker container stop $1
+    echo "$(dateTimeNow) - [INFO] - Stopping $1 container"
+    docker container stop $1
   fi
   
   nodeContainer=$(docker container ls --all | grep $1)
   if [[ $nodeContainer ]]; then
-      docker container rm $1
+    echo "$(dateTimeNow) - [INFO] - Removing $1 container"
+    docker container rm $1
   fi
 }
